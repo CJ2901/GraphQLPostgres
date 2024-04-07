894266ae15db92a0882e7fcc97515a0129aafb42
CJ2901 <christian.magallanes@alum.udep.edu.pe>
2024-04-07 01:10:48 -0500

    CRUD services done

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { ItemsResolver } from './items.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Item } from './entities/item.entity';
 
 @Module({
-  providers: [ItemsResolver, ItemsService],
+  providers: [
+      ItemsResolver
+    , ItemsService
+  ],
+  imports:[
+    TypeOrmModule.forFeature([Item]) // Esta configuración es para que se cargue ñla entidad a la base de datos
+  ]
 })
 export class ItemsModule {}
